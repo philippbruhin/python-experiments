56f1c54c68fa6c9b1ee05a930e69dc07caeb5b69
philippbruhin <info@philippbruhin.ch>
2018-11-11 21:07:25 +0100

    if else statements

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 age = 27
-
 name = "Philipp"
+todayIsCold = False
 
 print("Hello my name is {} and I am {} years old.".format(name, age))
+
+
+if age > 18:
+    print("You are older than 18")
+else:
+    print("You are younger than 18")
