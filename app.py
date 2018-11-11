e1d76332eaf72fd1aac03a0cd661a98fd5ccb012
philippbruhin <info@philippbruhin.ch>
2018-11-11 21:46:06 +0100

    lists

--- app.py
+++ app.py
@@ -15,5 +15,25 @@ def hello(thestring="default value"):
     print("Output of function: " + thestring)
 
 hello("Hello World!")
-hello("Hello World!")
 hello()
+
+dognames = ["Fido", "Sean", "Sally", "Hansdampf" , 43321, False, 3.1415]
+
+print(dognames)
+
+dognames.insert(0, "Jane")
+
+print(dognames)
+
+print(dognames[2])
+print(len(dognames))
+
+del(dognames[2])
+
+print(dognames)
+print(len(dognames))
+
+dognames[2] = "ReplaceName"
+
+print(dognames)
+print(len(dognames))
