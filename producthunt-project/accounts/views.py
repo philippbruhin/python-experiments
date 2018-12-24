7674d657167de1daef897442f70280c9462d3612
philippbruhin <info@philippbruhin.ch>
2018-12-24 21:41:06 +0100

    business logic sign up page

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        # User has info and wants an account now
+        if request.POST['password1'] == request.POST['password2']:
+            # Username already exists
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+            # Sign up and log that user in
+                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+
+    else:
+        # User wants to enter info
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
