d7a32861dc203f4e3b8dd4adb85fac407c9f6243
philippbruhin <info@philippbruhin.ch>
2018-12-24 22:59:40 +0100

    logout procedure

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -23,8 +23,19 @@ def signup(request):
         return render(request, 'accounts/signup.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'Username or password is incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
-    # TODO Need to route to homepage and logout
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+
     return render(request, 'accounts/signup.html')
